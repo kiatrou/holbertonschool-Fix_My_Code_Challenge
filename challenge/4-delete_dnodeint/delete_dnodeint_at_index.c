56487bae09ca61aba0472a129c7ec04f7cb30619
Kassandra <kassandraiatrou@gmail.com>
2025-09-30 14:39:36 +1000

    task 4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -45,7 +45,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     {
         (*head)->prev->next = (*head)->next;
         if ((*head)->next)
-            (*head)->next->prev = (*head)->prev;  // Update next node's prev pointer
+            (*head)->next->prev = (*head)->prev;
         free(*head);
         *head = saved_head;
     }
